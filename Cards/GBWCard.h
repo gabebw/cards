d6ac66e4c3d2c679f964d920224afaf11f857816
Gabe Berke-Williams <gabebw@gabebw.com>
2014-03-14 16:49:54 -0400

    Add GBWCard class

---
+++ Cards/GBWCard.h
@@ -0,0 +1,15 @@
+//
+//  GBWCard.h
+//  Cards
+//
+//  Created by Gabe Berke-Williams on 3/14/14.
+//  Copyright (c) 2014 Gabebw, inc. All rights reserved.
+//
+
+@import Foundation;
+
+@interface GBWCard : NSObject
+
+@property (strong) NSString *contents;
+
+@end
---
+++ Cards/GBWCard.m
@@ -0,0 +1,13 @@
+//
+//  GBWCard.m
+//  Cards
+//
+//  Created by Gabe Berke-Williams on 3/14/14.
+//  Copyright (c) 2014 Gabebw, inc. All rights reserved.
+//
+
+#import "GBWCard.h"
+
+@implementation GBWCard
+
+@end
