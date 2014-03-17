28c7db1559bb82f4dd70de519ae3b3b84f63e874
Gabe Berke-Williams <gabebw@gabebw.com>
2014-03-16 20:53:49 -0400

    Override init

--- Cards/GBWPlayingCardDeck.m
+++ Cards/GBWPlayingCardDeck.m
@@ -7,7 +7,28 @@
 //
 
 #import "GBWPlayingCardDeck.h"
+#import "GBWPlayingCard.h"
+
 
 @implementation GBWPlayingCardDeck
+
+// Override the `init` in [[MyClass alloc] init]
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self){
+        for(NSString *suit in [GBWPlayingCard validSuits]){
+            for(NSUInteger rank = 1; rank <= [GBWPlayingCard maxRank]; rank++){
+                GBWPlayingCard *card = [[GBWPlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
+    }
+    
+    return self;
+}
 
 @end
